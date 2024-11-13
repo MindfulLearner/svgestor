4b8f87b6f6191eca03a69851db08a3777f823e9c
MindfulLearner <dimajosh1009@gmail.com>
2024-11-13 02:14:35 +0100

    fix/fixed svgo modules
    svgo modules wasn't working, solved by delete node_modules and reinstall
    it

--- src/utils/transformUtils.tsx
+++ src/utils/transformUtils.tsx
@@ -1,10 +1,12 @@
+import fs from 'fs/promises';
 import { optimize } from 'svgo';
-import fs from 'fs';
 
-// optimize the svg file by reading the file and then using optimize from svgo that will use the config.mjs file
-async function optimizeSvgFile(filePath: string) {
-//TODO: adjust promises to run pnpm run build
-  const svgData = await fs.promises.readFile(filePath, 'utf8');
+/**
+ * optimize the svg file by reading the file and then using optimize from svgo that will use the svgo.config.mjs file
+ */
+async function optimizeSvgFile(filePath: string): Promise<string> {
+  //TODO: adjust promises to run pnpm run build
+  const svgData = await fs.readFile(filePath, 'utf8');
   const result = optimize(svgData, { path: filePath, multipass: true });
   return result.data;
 }
